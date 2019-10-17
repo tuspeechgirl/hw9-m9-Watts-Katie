b59a054907a68126727dd90dc80b45494539b1eb
Katie Watts <kathryn.watts@shipk12.org>
2019-10-17 19:55:42 -0400

    Add JS functionality and style page uniquely

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-var accordianHeaders =document.querySelectorAll('#accordianHeaders');
+var accordianHeaders = document.querySelectorAll('#accordian .accordian-headers');
+var accordianDescriptions = document.querySelectorAll('#accordian .accordian-description');
 
-function expandAccordianDescription(){
+//drop-down//
+function expandAccordianDescription() {
 
+//handles toggle//
+  for (i = 0; i < accordianHeaders.length; i++) {
+    accordianHeaders[i].childNodes[3].innerHTML = '<i class="fas fa-holly-berry"></i>';
+    accordianDescriptions[i].classList.remove('accordian-open');
+  }
+
+
+this.nextElementSibling.classList.toggle('accordian-open');
+this.childNodes[3].innerHTML = '<i class="fas fa-candy-cane"></i>';
 }
 
+//Add event click//
+for (i=0; i < accordianHeaders.length; i++) {
 
-for (i=0; i < accordianHeaders.length; i++){
-
-accordianHeaders.addEventListener('click', expandAccordianDescription);
+accordianHeaders[i].addEventListener('click', expandAccordianDescription);
 }
