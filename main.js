848f25ed4378722f53a8c68a6488402b82cea879
Katie Watts <kathryn.watts@shipk12.org>
2019-10-17 18:52:53 -0400

    Add content and style

---
+++ main.js
@@ -0,0 +1,11 @@
+var accordianHeaders =document.querySelectorAll('#accordianHeaders');
+
+function expandAccordianDescription(){
+
+}
+
+
+for (i=0; i < accordianHeaders.length; i++){
+
+accordianHeaders.addEventListener('click', expandAccordianDescription);
+}
